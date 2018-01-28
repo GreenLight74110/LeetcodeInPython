0918505e47b9d20d4b5def105c738d0facfcb8cd
hejunhua <jhhe_hn@foxmail.com>
2018-01-28 18:00:06 +0800

    merge_sort for linkedlist

--- medium/sort-list.py
+++ medium/sort-list.py
@@ -14,10 +14,45 @@ class ListNode:
         if self:
             return "{} -> {}".format(self.val, repr(self.next))
 
+
 class Solution:
+    def mergeSorted(self, l1, l2):
+        dummy = ListNode(0)
+        cur = dummy
+        while l1 is not None and l2 is not None:
+            if (l1.val < l2.val):
+                cur.next, cur, l1 = l1, l1, l1.next
+            else:
+                cur.next, cur, l2 = l2, l2, l2.next
+
+        if l1 is not None:
+            cur.next = l1
+        if l2 is not None:
+            cur.next = l2
+
+        return dummy.next
+
     # @param head, a ListNode
     # @return a ListNode
     def sortList(self, head):
+        if head is None or head.next is None:
+            return head
+
+        slow, mid, fast = head, None, head
+        while fast is not None and fast.next is not None:
+            slow, mid, fast = slow.next, slow, fast.next.next
+        mid.next = None
+
+        sored_l1 = self.sortList(head)
+        sored_l2 = self.sortList(slow)
+
+        return self.mergeSorted(sored_l1, sored_l2)
+
+    #   return self.mergeTwoLists2(self.sortList2(head), self.sortList2(slow))
+
+    # @param head, a ListNode
+    # @return a ListNode
+    def sortList2(self, head):
         if head == None or head.next == None:
             return head
 
@@ -26,12 +61,12 @@ class Solution:
             prev, fast, slow = slow, fast.next.next, slow.next
         prev.next = None
 
-        sorted_l1 = self.sortList(head)
-        sorted_l2 = self.sortList(slow)
+        sorted_l1 = self.sortList2(head)
+        sorted_l2 = self.sortList2(slow)
 
-        return self.mergeTwoLists(sorted_l1, sorted_l2)
+        return self.mergeTwoLists2(sorted_l1, sorted_l2)
 
-    def mergeTwoLists(self, l1, l2):
+    def mergeTwoLists2(self, l1, l2):
         dummy = ListNode(0)
 
         cur = dummy
@@ -48,9 +83,10 @@ class Solution:
 
         return dummy.next
 
+
 if __name__ == "__main__":
     head = ListNode(3)
     head.next = ListNode(4)
     head.next.next = ListNode(1)
-    head.next.next.next= ListNode(2)
+    head.next.next.next = ListNode(2)
     print(Solution().sortList(head))
